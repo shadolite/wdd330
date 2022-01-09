f7870cfad3f5daef78fa7d7fc2d3efe210e01677
shadolite <amy.kawaii@gmail.com>
2022-01-08 21:07:54 -0700

    Added js to load table of contents

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 const links = [
   {
-    label: "Week1 notes",
+    label: "Week 01",
     url: "week1/index.html"
   }
 ]
+
+const getLinksHTML = () => {
+  var html = '';
+
+  links.forEach(link => {
+    html += `\n<li><a href="${link.url}">${link.label}</a></li>`;
+  })
+
+  return html;
+}
+
+const loadTableOfOntents = () => {
+  var ol = document.getElementById("toc");
+  var html = getLinksHTML();
+  ol.innerHTML += html;
+}
+
+window.onload = loadTableOfOntents;
