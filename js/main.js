f9f1ec247db1869191122e29cc02bf1934729e27
shadolite <amy.kawaii@gmail.com>
2022-01-15 20:46:19 -0700

    Added files and notes for week 2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const links = [
   {
     label: "Week 01",
     url: "week01/index.html"
+  },
+  {
+    label: "Week 02",
+    url: "week02/index.html"
   }
 ]
 
@@ -9,16 +13,19 @@ const getLinksHTML = () => {
   var html = '';
 
   links.forEach(link => {
+    // let a = document.createElement(`a`);
+    // a.innerHTML = link.url;
+    // a.innerText = link.label;
     html += `\n<li><a href="${link.url}">${link.label}</a></li>`;
   })
 
   return html;
 }
 
-const loadTableOfOntents = () => {
+const loadTableOfContents = () => {
   var ol = document.getElementById("toc");
   var html = getLinksHTML();
   ol.innerHTML += html;
 }
 
-window.onload = loadTableOfOntents;
+window.onload = loadTableOfContents;
